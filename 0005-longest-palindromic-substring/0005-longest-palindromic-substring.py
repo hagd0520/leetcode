a6a282c39c751d9f82db06530bc6c2e0f2e8143a
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-02-25 14:37:23 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,7 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        for i in range(len(s))[::-1]:
+            for j in range(len(s)):
+                target = s[j : j + i + 1]
+                if target == target[::-1]:
+                    return target
