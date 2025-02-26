0741c88e649df7191ee980903dc6e9bb7d34235e
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-02-26 14:35:56 +0900

    Time: 98 ms (35.25%), Space: 18.6 MB (65.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,27 +2,27 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         # 모범 답안
-        vistied = defaultdict(int)
+        # vistied = defaultdict(int)
         
-        for i, num in enumerate(nums):
-            if target - num in vistied:
-                return [i, vistied[target - num]]
+        # for i, num in enumerate(nums):
+        #     if target - num in vistied:
+        #         return [i, vistied[target - num]]
             
-            vistied[num] = i
+        #     vistied[num] = i
         
         
         # 나의 답(투 포인터)
-        # temp = sorted(nums)
+        temp = sorted(nums)
         
-        # for i in range(len(temp))[::-1]:
-        #     for j in range(i):
-        #         result = temp[i] + temp[j]
+        for i in range(len(temp))[::-1]:
+            for j in range(i):
+                result = temp[i] + temp[j]
                 
-        #         if result == target:
-        #             left = nums.index(temp[j])
-        #             right = nums.index(temp[i])
+                if result == target:
+                    left = nums.index(temp[j])
+                    right = nums.index(temp[i])
                     
-        #             if left == right:
-        #                 right = nums.index(temp[i], left + 1)
+                    if left == right:
+                        right = nums.index(temp[i], left + 1)
                     
-        #             return [left, right]
+                    return [left, right]
