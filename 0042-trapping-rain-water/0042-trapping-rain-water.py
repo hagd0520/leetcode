6d19f3118ce8cfd2b8d98169744c808130275511
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-02-27 15:22:20 +0900

    Time: 4 ms (93.96%), Space: 19.5 MB (13.03%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,14 +1,18 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        def calculate(arr):
-            res = 0
-            hmax = 0
-            for h in arr:
-                if h < hmax:
-                    res += hmax - h
-                else:
-                    hmax = h
-            return res
+        max_h = height.index(max(height))
         
-        i = height.index(max(height))
-        return calculate(height[:i]) + calculate(height[i+1:][::-1])
+        left = height[:max_h + 1]
+        right = height[max_h:][::-1]
+        
+        def cal(arr: List[int]):
+            volume = 0
+            max_h = 0
+            for i in arr:
+                if i <= max_h:
+                    volume += max_h - i
+                else:
+                    max_h = i
+            return volume
+        
+        return cal(left) + cal(right)
