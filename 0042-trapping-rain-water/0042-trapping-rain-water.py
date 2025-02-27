ebb4067fea4f93318b849f18ca6744076ecbd03d
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-02-27 14:31:59 +0900

    Time: 9451 ms (5.1%), Space: 23.6 MB (6.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,42 +1,36 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        def get_filled_height(h: List[int]) -> List[int]:
-            h_dict = defaultdict(list)
-            hs = h[:]
-            
-            for i, j in enumerate(h):
-                h_dict[j].append(i)
-                
-            h_list = sorted(h_dict.items(), key=lambda item:item[0], reverse=True)
-                            
-            prev_left = 10**5
-            prev_right = 0
-            
-            for _h, indexes in h_list:
-                
-                left = indexes[0]
-                right = indexes[-1]
-                                
-                if left > prev_left:
-                    left = prev_left
-                    
-                if right < prev_right:
-                    right = prev_right
-                
-                for i in range(left, right):
-                    if hs[i] < _h:
-                        hs[i] = _h
-                
-                prev_left = left
-                prev_right = right
-            
-            return hs
-                
-        filled_height = get_filled_height(height)
-                
         filled_count = 0
         
-        for i in range(len(height)):
-            filled_count += filled_height[i] - height[i]
+        h_dict = defaultdict(list)
+        hs = height[:]
+        
+        for i, j in enumerate(height):
+            h_dict[j].append(i)
             
+        h_list = sorted(h_dict.items(), key=lambda item:item[0], reverse=True)
+                        
+        prev_left = 10**5
+        prev_right = 0
+        
+        for _h, indexes in h_list:
+            
+            left = indexes[0]
+            right = indexes[-1]
+                            
+            if left > prev_left:
+                left = prev_left
+                
+            if right < prev_right:
+                right = prev_right
+            
+            for i in range(left, right):
+                if hs[i] < _h:
+                    filled_count += _h - hs[i]
+                    hs[i] = _h
+                    
+            
+            prev_left = left
+            prev_right = right
+        
         return filled_count
