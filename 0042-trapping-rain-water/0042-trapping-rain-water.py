35420b2a81c2450324280a1968c722d08b7cb4e5
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-02-27 14:39:53 +0900

    Time: 63 ms (5.1%), Space: 19.2 MB (93.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,36 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        filled_count = 0
+        if not height:
+            return 0
         
-        h_dict = defaultdict(list)
-        hs = height[:]
+        volume = 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
         
-        for i, j in enumerate(height):
-            h_dict[j].append(i)
+        while left < right:
+            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+            print(left_max, "and", right_max)
             
-        h_list = sorted(h_dict.items(), key=lambda item:item[0], reverse=True)
-                        
-        prev_left = 10**5
-        prev_right = 0
-        
-        for _h, indexes in h_list:
-            
-            left = indexes[0]
-            right = indexes[-1]
-                            
-            if left > prev_left:
-                left = prev_left
-                
-            if right < prev_right:
-                right = prev_right
-            
-            for i in range(left, right):
-                if hs[i] < _h:
-                    filled_count += _h - hs[i]
-                    hs[i] = _h
-                    
-            
-            prev_left = left
-            prev_right = right
-        
-        return filled_count
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
+            else:
+                volume += right_max - height[right]
+                right -= 1
+        return volume
