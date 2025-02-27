20ddcabc78c5828f5d53ae4d39faf5c717803c1d
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-02-27 15:22:26 +0900

    Time: 10 ms (77.19%), Space: 19.3 MB (40%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -2,12 +2,10 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         max_h = height.index(max(height))
         
-        left = height[:max_h + 1]
-        right = height[max_h:][::-1]
-        
         def cal(arr: List[int]):
             volume = 0
             max_h = 0
+            
             for i in arr:
                 if i <= max_h:
                     volume += max_h - i
@@ -15,4 +13,4 @@ class Solution:
                     max_h = i
             return volume
         
-        return cal(left) + cal(right)
+        return cal(height[:max_h + 1]) + cal(height[max_h:][::-1])
