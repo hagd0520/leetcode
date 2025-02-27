7c92271abf48642c7b7dcadd9946ec4a5d20de7f
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-02-27 14:01:08 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0974-reorder-data-in-log-files/0974-reorder-data-in-log-files.py
+++ 0974-reorder-data-in-log-files/0974-reorder-data-in-log-files.py
@@ -1,11 +1,31 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        letters, digits = [], []
-        for log in logs:
-            if log.split()[1].isdigit():
-                digits.append(log)
-            else:
-                letters.append(log)
+        nums = deque()
+        for i in range(len(logs), 0, -1):
+            for j in range(i):
+                if (j + 1) == len(logs):
+                    break
+                if re.match("[0-9]", logs[j].split()[1]):
+                    nums.appendleft((logs.pop(j)))
+                if self.isGreater(logs[j], logs[j + 1]):
+                    logs[j], logs[j + 1] = logs[j + 1], logs[j]
+        logs = logs + list(nums)
+        return logs
+            
+    def isGreater(self, origin: str, new: str):
+        s_origin = origin.split()
+        s_new = new.split()
+        
+        count = len(s_origin) if len(s_origin) < len(s_new) else len(s_new)
+        
+        for i in range(1, count):
+            inner_count = len(s_origin[i]) if len(s_origin[i]) < len(s_new[i]) else len(s_new[i])
+            
+            for j in range(inner_count):
+                if ord(s_origin[i][j]) > ord(s_new[i][j]):
+                    return True
+                if ord(s_origin[i][j]) < ord(s_new[i][j]):
+                    return False
                 
-        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
-        return letters + digits
+            if len(s_origin) != len(s_new):
+                return False if len(s_origin) < len(s_new) else True
