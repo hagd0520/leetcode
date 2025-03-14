750f955029631189835ace3b70a272807a2c786f
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-03-14 14:46:06 +0900

    Time: 90 ms (99.36%), Space: 21.4 MB (14.35%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,38 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
         nums.sort()
-        ans = []
-        n = len(nums)
-        for i in range(n):
-            if nums[i] > 0:
-                break
-            if i and nums[i] == nums[i - 1]:
+        counts = Counter(nums)
+        zero_count = counts.pop(0, 0)
+        result = [[0, 0, 0]] if zero_count >= 3 else []  # The only triplet with all duplicates
+        unique = list(counts)
+    
+        # Find all remaining triplets that include 0
+        if zero_count:
+            for num in unique:
+                if num >= 0:
+                    break
+                if -num in counts:
+                    result.append([num, 0, -num])
+    
+        # Find all triplets with two duplicates
+        for num in unique:
+            if num % 2 != 0:
                 continue
-            j, k = i + 1, n - 1
-            while j < k:
-                tots = nums[i] + nums[j] + nums[k]
-
-                if tots > 0:
-                    k -= 1
-                elif tots < 0:
-                    j += 1
-                else:
-                    ans.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    while nums[j] == nums[j - 1] and j < k:
-                        j += 1
-        return ans
+            candidate = -num // 2
+            if counts[candidate] >= 2:
+                result.append([num, candidate, candidate])
+    
+        # Find all remaining triplets with no duplicates
+        if unique:
+            start = bisect_right(unique, max(-unique[-1] // 2, unique[0]))
+            stop = bisect_right(unique, min(-unique[0] // 2, unique[-1]))
+            for i in range(start, stop):
+                num = unique[i]
+                j = bisect_right(unique, -num * 2) if num < 0 else i + 1
+                k = bisect_right(unique, -unique[0] - num)
+                for right in unique[j:k]:
+                    left = -num - right
+                    if left in counts:
+                        result.append([left, num, right])
+    
+        return result
