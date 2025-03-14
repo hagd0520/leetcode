58b120f7550370bd4fc903181a99c380bb9e371a
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-03-14 15:11:43 +0900

    Time: 35 ms (21.11%), Space: 19.7 MB (34.67%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,5 +1,10 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
+        answer = 0
         
-        return nums[len(nums) - 4] + nums[len(nums) - 2]
+        for i, num in enumerate(nums):
+            if i % 2 == 0:
+                answer += num
+        
+        return answer
