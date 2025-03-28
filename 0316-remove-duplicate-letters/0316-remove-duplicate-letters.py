89a0407fe1c9e8ebc1b5a5ad5ab778ae763b831d
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-03-28 16:35:05 +0900

    Time: 4 ms (31.58%), Space: 18 MB (7.3%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,18 +1,28 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
+        
+        # 스택 
         last = {c : i for i, c in enumerate(s)}
-        stack = []
-        seen = set()
-
-        for index, c in enumerate(s):
-            if c in seen:
+        seen, stack = set(), []
+        
+        for index, char in enumerate(s):
+            if char in seen:
                 continue
-
-            while stack and index < last[stack[-1]] and c < stack[-1]:
-                a = stack.pop()
-                seen.remove(a)
-
-            stack.append(c)
-            seen.add(c)
-
-        return "".join(stack)
+            
+            while stack and char < stack[-1] and last[stack[-1]] > index:
+                seen.remove(stack.pop())
+                
+            stack.append(char)
+            seen.add(char)
+            
+        return ''.join(stack)
+        
+        # 재귀
+        
+        # for i in sorted(set(s)):
+        #     suffix = s[s.index(i):]
+            
+        #     if set(suffix) == set(s):
+        #         return i + self.removeDuplicateLetters(suffix.replace(i, ""))
+            
+        # return ""
