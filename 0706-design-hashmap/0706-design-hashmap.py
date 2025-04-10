7e81726801d59a5e2614e3d7aea846c353cbe691
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-04-10 15:09:25 +0900

    Time: 71 ms (31.18%), Space: 23.1 MB (24.31%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -9,7 +9,7 @@ class MyHashMap:
 
     def __init__(self):
         self.size = 10
-        self.list: List[ListNode] = [ListNode()] * self.size
+        self.list: List[ListNode] = [ListNode() for _ in range(self.size)]
         self.load_factor = 0.75
         self.count = 0
         
@@ -25,10 +25,23 @@ class MyHashMap:
             
             if exist_node.next is None:
                 exist_node.next = ListNode(key, value)
+                self.count += 1
+                
+                if self.isOverloaded():
+                    old_size = self.size
+                    old_list = self.list
+                    self.size *= 10
+                    self.list = [ListNode() for _ in range(self.size)]
+                    
+                    for i in range(old_size):
+                        node = old_list[i]
+                        while node.next:
+                            self.put(node.next.key, node.next.value)
+                            node = node.next
+                
                 return
 
             exist_node = exist_node.next
-                    
 
     def get(self, key: int) -> int:
         hashed_key = self.hash(key)
@@ -46,7 +59,7 @@ class MyHashMap:
             
         return -1
 
-        
+                    
     def remove(self, key: int) -> None:
         hashed_key = self.hash(key)
         exist_node = self.list[hashed_key]
@@ -55,14 +68,19 @@ class MyHashMap:
         while exist_node:
             if exist_node.key == key:
                 prev_node.next = prev_node.next.next
+                self.count -= 1
                 return
             
             prev_node = exist_node
             exist_node = exist_node.next
 
-        
+
     def hash(self, key):
         return key % self.size
+
+
+    def isOverloaded(self) -> bool:
+        return self.size * self.load_factor < self.count
 
 
 # Your MyHashMap object will be instantiated and called as such:
