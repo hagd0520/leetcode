bda1abbb60c0923dd90c9d1414b412013953e259
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-04-11 15:20:12 +0900

    Time: 90 ms (24.55%), Space: 23.2 MB (23.47%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -14,6 +14,8 @@ class MyHashMap:
         self.count = 0
         
 
+    # 새로운 키와 값을 입력합니다. 
+    # 만약 로드 팩터를 기준으로 과적되어 있을 경우 리사이징을 진행합니다.
     def put(self, key: int, value: int) -> None:
         self.do_put(key, value)
                 
@@ -21,6 +23,8 @@ class MyHashMap:
             self.resize()
             
 
+    # 입력해둔 데이터 중 인수로 받은 키와 일치하는 데이터의 값을 반환합니다.
+    # 키가 일치하는 데이터가 없을 경우 -1을 반환합니다.
     def get(self, key: int) -> int:
         node = self.find_node_by_key(key)
         
@@ -30,6 +34,8 @@ class MyHashMap:
         return -1
     
 
+    # 입력해둔 데이터 중 인수로 받은 키와 일치하는 데이터를 제거합니다.
+    # 참조값을 지움으로써 GC에게 수거됩니다.
     def remove(self, key: int) -> None:
         prev_node = self.find_prev_node_by_key(key)
         
@@ -37,7 +43,8 @@ class MyHashMap:
             prev_node.next = prev_node.next.next
             
 
-    def do_put(self, key, value):
+    # 새로운 키와 값을 입력합니다.
+    def do_put(self, key, value) -> None:
         prev_node = self.find_prev_node_by_key(key)
         
         if prev_node.next is None:
@@ -48,7 +55,8 @@ class MyHashMap:
             prev_node.next.value = value
             
 
-    def resize(self):
+    # 데이터 용량을 열배 늘리고 기존의 데이터들을 이주시킵니다.
+    def resize(self) -> None:
         old_size = self.size
         old_list = self.list
         self.size *= 10
@@ -62,19 +70,23 @@ class MyHashMap:
                 node = node.next
 
 
-    def hash(self, key):
+    # 간단한 해시 함수입니다.
+    def hash(self, key) -> int:
         return key % self.size
 
 
+    # 데이터가 과적되었는지 여부를 판별합니다.
     def isOverloaded(self) -> bool:
         return self.size * self.load_factor < self.count
     
     
-    def find_node_by_key(self, key):
+    # 키에 부합하는 데이터의 노드를 반환합니다.
+    def find_node_by_key(self, key) -> Optional[ListNode]:
         return self.find_prev_node_by_key(key).next
 
 
-    def find_prev_node_by_key(self, key):
+    # 키에 부합하는 데이터의 이전 노드를 반환합니다.
+    def find_prev_node_by_key(self, key) -> ListNode:
         hashed_key = self.hash(key)
         node = self.list[hashed_key]
         prev_node = None
