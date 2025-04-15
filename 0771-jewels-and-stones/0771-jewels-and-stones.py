8a276c1302f9c56e43eb4be80fd29b5f7567d167
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-04-15 15:18:47 +0900

    Time: 0 ms (100%), Space: 17.7 MB (82.91%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -2,5 +2,3 @@ class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         jewels_set = set(jewels)
         return len([i for i in stones if i in jewels_set])
-                
-        return answer
