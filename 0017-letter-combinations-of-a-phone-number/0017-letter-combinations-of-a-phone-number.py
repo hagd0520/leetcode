87256868155c0ee363bce75af6fc4aeda2f0bc50
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-04-17 14:49:47 +0900

    Time: 0 ms (100%), Space: 17.8 MB (68.62%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
 
+        # 모범 답안
         def dfs(index, path):
             if len(path) == len(digits):
                 result.append(path)
@@ -28,3 +29,36 @@ class Solution:
         dfs(0, "")
         
         return result
+        
+        # 나의 답
+        # answer = []
+        
+        # letters_dict = {
+        #     "2": "abc",
+        #     "3": "def",
+        #     "4": "ghi",
+        #     "5": "jkl",
+        #     "6": "mno",
+        #     "7": "pqrs",
+        #     "8": "tuv",
+        #     "9": "wxyz"
+        # }
+
+        # def concat(letter: str, last_digits: List[str]):
+        #     if not last_digits:
+        #         if letter:
+        #             answer.append(letter)
+        #         return
+
+        #     next_digits = last_digits[1:]
+            
+        #     for i in letters_dict[last_digits[0]]:
+        #         next_letter = letter + i
+        #         concat(next_letter, next_digits)
+        
+        
+        # digits_list = list(digits)
+        
+        # concat("", digits_list)
+        
+        # return answer
