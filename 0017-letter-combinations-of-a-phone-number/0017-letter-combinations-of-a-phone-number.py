a511a29f13dc2d8d79f34141030b6a99c5003e21
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-04-17 14:24:39 +0900

    Time: 0 ms (100%), Space: 17.9 MB (23.24%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,33 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        if not digits:
+            return []
+
+        answer = []
+        letters_dict = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        def concat(letter: str, last_digits: List[str]):
+            if not last_digits:
+                answer.append(letter)
+                return
+                
+            for i in letters_dict[last_digits[0]]:
+                next_letter = letter + i
+                concat(next_letter, last_digits[1:])
+        
+        
+        digits_list = list(digits)
+        
+        concat("", digits_list)
+        
+        return answer
