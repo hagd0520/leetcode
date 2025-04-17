ac09be293be3a54e5a9eb962d6d0d9873eeafecb
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-04-17 15:45:52 +0900

    Time: 0 ms (100%), Space: 18 MB (32.6%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        answer = []
+
+        def dfs(indexes: List[int], permutation: List[int] = []):
+
+            if not indexes:
+                answer.append(permutation)
+            
+            for i in range(len(indexes)):
+                new_indexes = indexes[:]
+                new_permutation = permutation[:]
+                
+                new_permutation.append(nums[new_indexes.pop(i)])
+                dfs(new_indexes, new_permutation)
+                
+        dfs(list(range(len(nums))))
+        
+        return answer
