eb2c8605fc27e134134548a3eefe4abe76c6f166
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-04-29 14:15:21 +0900

    Time: 152 ms (29.43%), Space: 59.8 MB (16.08%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        answer = []
+        candidate = []
+
+        nums = list(range(1, n + 1))
+
+        def dfs(index=0):
+            if len(candidate) < k:
+                for i in range(index, len(nums)):
+                    candidate.append(nums[i])
+                    dfs(i + 1)
+            else:
+                if len(candidate) >= k:
+                    answer.append(candidate[:])
+
+            if candidate:
+                candidate.pop()
+
+        dfs()
+
+        return answer
