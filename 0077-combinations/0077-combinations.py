5afbae4da3712f2bcc0f6fc4de1d0ada22f5a5d3
hagd0520 <146146134+hagd0520@users.noreply.github.com>
2025-04-29 14:17:07 +0900

    Time: 147 ms (33.79%), Space: 59.7 MB (28.38%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,22 +1,23 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        answer = []
-        candidate = []
+        return list(itertools.combinations(list(range(1, n + 1)), k))
+        # answer = []
+        # candidate = []
 
-        nums = list(range(1, n + 1))
+        # nums = list(range(1, n + 1))
 
-        def dfs(index=0):
-            if len(candidate) < k:
-                for i in range(index, len(nums)):
-                    candidate.append(nums[i])
-                    dfs(i + 1)
-            else:
-                if len(candidate) >= k:
-                    answer.append(candidate[:])
+        # def dfs(index=0):
+        #     if len(candidate) < k:
+        #         for i in range(index, len(nums)):
+        #             candidate.append(nums[i])
+        #             dfs(i + 1)
+        #     else:
+        #         if len(candidate) >= k:
+        #             answer.append(candidate[:])
 
-            if candidate:
-                candidate.pop()
+        #     if candidate:
+        #         candidate.pop()
 
-        dfs()
+        # dfs()
 
-        return answer
+        # return answer
